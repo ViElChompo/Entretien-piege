4fb77cf554b71bed7bacf4243e9c63b96c255c45
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-05-12 10:41:05 +0200

    added user seeder, and roles column in db.

---
+++ entretien-piege-backend/database/migrations/2025_05_12_083338_add_role_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user'); // Ajouter une colonne role avec une valeur par défaut 'user'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role'); // Supprimer la colonne role si la migration est annulée
+        });
+    }
+};
---
+++ entretien-piege-backend/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('fullstache1234!'), // Remplacez par un mot de passe sécurisé
+            'role' => 'admin', // Spécifier que cet utilisateur est admin
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
