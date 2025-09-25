55fb3c5d0bdb1eeab6c4ba1852547a24e318fe90
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-09-25 15:28:50 +0200

    added tailwindcss

--- entretien-piege-frontend/src/main.js
+++ entretien-piege-frontend/src/main.js
@@ -1,11 +1,16 @@
+import './assets/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './axios' // Importer la configuration d'Axios
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'       // thème moderne
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
+app.use(PrimeVue, { theme: { preset: Aura } }) 
 app.mount('#app')
 
